d1c786098b23a625a2e163a441b8640a9ea34719
Panayiotis Georgiou <georgioupanayiotis@users.noreply.github.com>
2019-11-04 12:47:16 +0000

    Create MainActivity.kt

---
+++ Splash Screen/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.panayiotisgeorgiou.splashscreenwithkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
