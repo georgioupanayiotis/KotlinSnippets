94eab453e495606dd9792e57f8bc51212423bfcf
Panayiotis Georgiou <georgioupanayiotis@users.noreply.github.com>
2019-11-25 13:14:46 +0000

    Create HomeActivity.kt

---
+++ InternetConnectivity/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.panayiotisgeorgiou.intrnetconnectivity
+
+import android.os.Bundle
+
+class HomeActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
